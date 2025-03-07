e927d0e4cf873dbd30dbf484aef3ea2c023511e1
Ruslan Nemchaninov <89201003349@mail.ru>
2025-03-07 14:01:17 +0300

    First ver.

---
+++ DataStructuresDotNetUnitTestProject/OpenAddressHashTableUnitTest.cs
@@ -0,0 +1,383 @@
+﻿using HashTablesLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataStructuresTestProject
+{
+    [TestClass]
+    public class OpenAddressHashTableUnitTest
+    {
+        [TestMethod]
+        public void Initialization_Correct()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            Assert.IsNotNull(hashTable);
+        }
+        [TestMethod]
+        public void Count_EqualsZeroAfterInitialization()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var expectedCount = 0;
+            Assert.AreEqual(expectedCount, hashTable.Count);
+        }
+        [TestMethod]
+        public void Count_EqualsZeroAfterClearing()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable.Add(item, item);
+                }
+            }
+            hashTable.Clear();
+            var expectedCount = 0;
+            Assert.AreEqual(expectedCount, hashTable.Count);
+        }
+        [TestMethod]
+        public void Count_IncreasesAfterAdding()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            var expectedCount = 0;
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable.Add(item, item);
+                    expectedCount++;
+                    Assert.AreEqual(expectedCount, hashTable.Count);
+                }
+            }
+        }
+        [TestMethod]
+        public void Add_NonExistentKey_PairAdded()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable.Add(item, item);
+                    Assert.IsTrue(array.Contains(item));
+                }
+            }
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_DuplicateKey_ThrowsException()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable.Add(item, item);
+                }
+            }
+            for (int i = 0; i < size / 2; i++)
+            {
+                hashTable.Add(array[i], array[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Remove_ExistingKey_PairRemoved()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable.Add(item, item);
+                }
+            }
+            for (int i = 0; i < size / 2; i++)
+            {
+                hashTable.Remove(array[i]);
+                Assert.IsFalse(hashTable.ContainsKey(array[i]));
+            }
+        }
+        [TestMethod]
+        public void Remove_NonExistentKey_ReturnsFalse()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable.Add(item, item);
+                }
+            }
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    Assert.IsFalse(hashTable.Remove(item));
+                }
+            }
+        }
+        [TestMethod]
+        public void Indexer_GetExistingKey_ValueTaken()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable.Add(item, item + 1);
+                }
+            }
+            for (int i = 0; i < size / 2; i++)
+            {
+                Assert.AreEqual(array[i] + 1, hashTable[array[i]]);
+            }
+        }
+        [TestMethod]
+        [ExpectedException(typeof(KeyNotFoundException))]
+        public void Indexer_GetNonExistentKey_ThrowsException()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable.Add(item, item);
+                }
+            }
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    var someKey = hashTable[item];
+                }
+            }
+        }
+        [TestMethod]
+        public void Indexer_SetNonExistentKey_ItemAdded()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable[item] = item;
+                    Assert.IsTrue(hashTable.ContainsKey(item));
+                }
+            }
+        }
+        [TestMethod]
+        public void Indexer_SetExistingKey_ItemEdited()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable.Add(item, item);
+                }
+            }
+            for (int i = 0; i < size / 2; i++)
+            {
+                hashTable[array[i]] = array[i] + 1;
+                Assert.AreEqual(array[i] + 1, hashTable[array[i]]);
+            }
+        }
+        [TestMethod]
+        public void TryGetValue_ExistingKey_ValueTakenAndCorrect()
+        {
+
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable.Add(item, item + 1);
+                }
+            }
+            int takenValue;
+            for (int i = 0; i < size / 2; i++)
+            {
+                Assert.IsTrue(hashTable.TryGetValue(array[i], out takenValue));
+                Assert.AreEqual(array[i] + 1, takenValue);
+            }
+        }
+        [TestMethod]
+        public void TryGetValue_NonExistentKey_ReturnsFalse()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable.Add(item, item + 1);
+                }
+            }
+            int takenValue;
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    Assert.IsFalse(hashTable.TryGetValue(item, out takenValue));
+                }
+            }
+        }
+        [TestMethod]
+        public void CopyTo_ItemsCopied()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable.Add(item, item);
+                }
+            }
+            KeyValuePair<int, int>[] newArray = new KeyValuePair<int, int>[size + 1];
+            hashTable.CopyTo(newArray, 1);
+            for (int i = 0; i < size / 2; i++)
+            {
+                Assert.IsTrue(newArray.Contains(new KeyValuePair<int, int>(array[i], array[i])));
+            }
+        }
+        [TestMethod]
+        public void Clear_ItemsDeleted()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable.Add(item, item);
+                }
+            }
+            hashTable.Clear();
+            for (int i = 0; i < size / 2; i++)
+            {
+                Assert.IsFalse(hashTable.ContainsKey(array[i]));
+            }
+        }
+        [TestMethod]
+        public void Keys_ShouldReturnAllKeys()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable.Add(item, item);
+                }
+            }
+            for (int i = 0; i < size / 2; i++)
+            {
+                Assert.IsTrue(hashTable.Keys.Contains(array[i]));
+            }
+        }
+        [TestMethod]
+        public void Values_ShouldReturnAllValues()
+        {
+            int size = 10103;
+            var hashTable = new OpenAddressHashTable<int, int>(size);
+            var array = new int[size / 2];
+            var rnd = new Random();
+            for (int i = 0; i < size / 2; i++)
+            {
+                var item = rnd.Next();
+                if (!array.Contains(item))
+                {
+                    array[i] = item;
+                    hashTable.Add(item, item * 2);
+                }
+            }
+            for (int i = 0; i < size / 2; i++)
+            {
+                Assert.IsTrue(hashTable.Values.Contains(array[i] * 2));
+            }
+        }
+    }
+}
---
+++ ExperimentsConsoleApp/Program.cs
@@ -0,0 +1,149 @@
+﻿using HashTablesLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace ExperimentsConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputText = File.ReadAllText("WarAndWorld.txt");
+            string pattern = @"\b\w+\b";
+            var matches = Regex.Matches(inputText, pattern);
+            var words = new List<string>();
+            foreach (Match match in matches)
+            {
+                words.Add(match.Value.ToLower());
+            }
+            Work_HashTable(words.ToArray());
+            Work_Dictionary(words.ToArray());
+        }
+        static void Work_HashTable(string[] words)
+        {
+            var hashTable = new OpenAddressHashTable<string, int>();
+            var watch = new Stopwatch();
+            #region Starting up
+            hashTable.Add("word", 0);
+            hashTable.ContainsKey("word");
+            hashTable.Remove("word");
+            #endregion
+
+            #region HashTable
+
+            Console.WriteLine("HashTable Insert");
+
+            watch.Restart();
+            // Вставка элементов в HashTable
+            foreach (var word in words)
+            {
+                if (hashTable.ContainsKey(word))
+                {
+                    hashTable[word]++;
+                    continue;
+                }
+                hashTable[word] = 1;
+            }
+            watch.Stop();
+            Console.WriteLine($"Time: {watch.Elapsed}");
+
+            Console.WriteLine("\n============\n");
+
+            Console.WriteLine("HashTable Taking Most Frequent Words");
+
+            var mostFrequentWords = new List<string>();
+            var frequency = 27;
+            watch.Restart();
+            // Получение выборки слов которые встречаются чаще 27 раз
+            foreach (var pair in hashTable)
+            {
+                if (pair.Value > frequency)
+                {
+                    mostFrequentWords.Add(pair.Key);
+                }
+            }
+            watch.Stop();
+            Console.WriteLine($"Time: {watch.Elapsed}");
+
+            Console.WriteLine("\n============\n");
+
+            Console.WriteLine("HashTable Deleting Most Frequent Words");
+
+            watch.Restart();
+            // Удаление слов которые встречаются чаще 27 раз
+            foreach (var word in mostFrequentWords)
+            {
+                hashTable.Remove(word);
+            }
+            watch.Stop();
+            Console.WriteLine($"Time: {watch.Elapsed}");
+
+            Console.WriteLine("\n============\n");
+
+            #endregion
+        }
+        static void Work_Dictionary(string[] words)
+        {
+            var dictionary = new System.Collections.Generic.Dictionary<string, int>();
+            var watch = new Stopwatch();
+
+            #region Dictionary
+
+            Console.WriteLine("Dictionary Insert");
+
+            watch.Restart();
+            // Вставка элементов в Dictionary
+            foreach (var word in words)
+            {
+                if (dictionary.ContainsKey(word))
+                {
+                    dictionary[word]++;
+                    continue;
+                }
+                dictionary[word] = 1;
+            }
+            watch.Stop();
+            Console.WriteLine($"Time: {watch.Elapsed}");
+
+            Console.WriteLine("\n============\n");
+
+            Console.WriteLine("Dictionary Taking Most Frequent Words");
+
+            var mostFrequentWords = new List<string>();
+            var frequency = 27;
+            watch.Restart();
+            // Получение выборки слов которые встречаются чаще 27 раз
+            foreach (var pair in dictionary)
+            {
+                if (pair.Value > frequency)
+                {
+                    mostFrequentWords.Add(pair.Key);
+                }
+            }
+            watch.Stop();
+            Console.WriteLine($"Time: {watch.Elapsed}");
+
+            Console.WriteLine("\n============\n");
+
+            Console.WriteLine("Dictionary Deleting Most Frequent Words");
+
+            watch.Restart();
+            // Удаление слов которые встречаются чаще 27 раз
+            foreach (var word in mostFrequentWords)
+            {
+                dictionary.Remove(word);
+            }
+            watch.Stop();
+            Console.WriteLine($"Time: {watch.Elapsed}");
+
+            Console.WriteLine("\n============\n");
+
+            #endregion
+        }
+    }
+}
